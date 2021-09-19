8f6a4a877b8ef090405f4440efb08daee0d10033
김동윤 <85720792+kdy9416@users.noreply.github.com>
2021-09-19 14:47:11 +0900

    부트 프로젝트 생성후 jsp연동

---
+++ login/src/main/java/com/example/login/LoginApplication.java
@@ -0,0 +1,25 @@
+package com.example.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@SpringBootApplication
+public class LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApplication.class, args);
+	}
+	
+	 @Bean
+	 public InternalResourceViewResolver setupViewResolver() {
+	 
+	        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+	 
+	        resolver.setPrefix("/WEB-INF/views/");
+	        resolver.setSuffix(".jsp");
+	        return resolver;
+	 }
+
+}
