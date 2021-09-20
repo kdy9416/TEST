699f080c5a45bdcd56ed1f99171f2e75accf882e
김동윤 <85720792+kdy9416@users.noreply.github.com>
2021-09-20 14:49:01 +0900

    sql password타입변경

--- login/src/main/resources/sql/User.sql
+++ login/src/main/resources/sql/User.sql
@@ -1,6 +1,6 @@
 Create table login_user(
 	id varchar(100) primary key,
-    password varchar(20) NOT NULL,
+    password varchar(100) NOT NULL,
     name varchar(20) NOT NULL,
     join_date Timestamp default now()
 	
