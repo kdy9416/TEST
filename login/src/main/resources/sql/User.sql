a40343005734a98dba0755b13193e1ae774f795b
김동윤 <85720792+kdy9416@users.noreply.github.com>
2021-09-21 18:37:45 +0900

    자동로그인을 위한 sql 컬럼 추가

--- login/src/main/resources/sql/User.sql
+++ login/src/main/resources/sql/User.sql
@@ -5,3 +5,7 @@ Create table login_user(
     join_date Timestamp default now()
 	
 );
+
+//자동로그인을 위한 컬럼 추가
+alter table login_user add column session_id varchar(100) not null default "none";
+alter table login_user add column limit_date timestamp;
